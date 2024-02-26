9ddb18c0e3d715af078f15115066f20f01666909
a-r3n <alec.renehan@outlook.com>
2024-02-26 20:55:42 +1100

    Added additional code to console log an error with homepage routing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,56 +4,28 @@ const { User } = require('../models/User');
 const { Comment } = require('../models/Comment');
 const withAuth = require('../utils/auth');
 
-
-// Route to render the homepage
-router.get('/', async (req, res) => {
-    try {
-        // Fetch all blog posts and JOIN with user data
-        const postData = await Post.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['name'],
-                },
-            ],
-        });
-
-        // Serialize data so the template can read it
-        const posts = postData.map((post) => post.get({ plain: true }));
-
-        // Pass serialized data and session flag into template
-        res.render('homepage', { 
-            posts, 
-            logged_in: req.session.logged_in 
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+// Temporarily modified route to render the homepage
+router.get('/', (req, res) => {
+    res.send('Homepage is under construction.');
 });
 
-// Route to render the login page
+// Keep the rest of your routes as they are
 router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect to the homepage
     if (req.session.logged_in) {
         res.redirect('/');
         return;
     }
-
     res.render('login');
 });
 
-// Route to render the signup page
 router.get('/signup', (req, res) => {
-    // If the user is already logged in, redirect to the homepage
     if (req.session.logged_in) {
         res.redirect('/');
         return;
     }
-
     res.render('signup');
 });
 
-// Route to render a single post by id
 router.get('/post/:id', async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
@@ -71,7 +43,6 @@ router.get('/post/:id', async (req, res) => {
 
         if (postData) {
             const post = postData.get({ plain: true });
-
             res.render('post', {
                 ...post,
                 logged_in: req.session.logged_in
